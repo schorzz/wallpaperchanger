d6b91a02879b49c2e5e4554257fe7fb86fec038e
Höling <Hling@192.168.0.42>
2016-09-18 20:57:44 +0200

    Klasse Bild bearbeitet neue Methoden: -Konstruktor -public void errechne() -public Integer getMittelwert()
    neue Instanzvariablen

--- src/adt/Bild.java
+++ src/adt/Bild.java
@@ -1,5 +1,32 @@
 package adt;
 
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
 public class Bild {
+	
+	private Image bild;
+	private Color farbmitte;
+	private Integer mittelwert = 0;
+	
+	public Bild(String pfad){
+		
+		if(pfad != ""){
+			
+			bild = new Image(pfad);
+			
+			
+		}
+		
+	}
+	public void errechne(){
+		
+	}
+	public Integer getMittelwert(){
+		
+		return mittelwert;
+	}
+	
+	
 
 }
